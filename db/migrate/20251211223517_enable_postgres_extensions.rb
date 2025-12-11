3b513154dbc29a2ebc78fcf50b043d8a70ee7af2
Robert Golusinski <robertgolusinski@gmail.com>
2025-12-11 23:36:19 +0100

    Make pgvector extension optional in migration
    - Handle case where pgvector is not installed on PostgreSQL server
    - Log warning and continue migration without vector extension
    - Embeddings will be added in future when pgvector is available

--- db/migrate/20251211223517_enable_postgres_extensions.rb
+++ db/migrate/20251211223517_enable_postgres_extensions.rb
@@ -4,6 +4,15 @@ class EnablePostgresExtensions < ActiveRecord::Migration[7.1]
     enable_extension "pg_trgm" unless extension_enabled?("pg_trgm")
     
     # Enable vector extension for pgvector (embeddings/semantic search)
-    enable_extension "vector" unless extension_enabled?("vector")
+    # Note: This requires pgvector to be installed on the PostgreSQL server
+    # For now, we'll enable it conditionally - it will be added when pgvector is installed
+    begin
+      enable_extension "vector" unless extension_enabled?("vector")
+    rescue ActiveRecord::StatementInvalid => e
+      # If vector extension is not available, log warning and continue
+      Rails.logger.warn "pgvector extension not available: #{e.message}"
+      # Migration will continue without vector extension
+      # Embeddings will be added in a future migration when pgvector is installed
+    end
   end
 end
